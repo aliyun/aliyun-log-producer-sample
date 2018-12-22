50d28784c239d4f91be9e7d05bd1131e971e8de5
brucewu-fly <brucewu.fly.cn@gmail.com>
2018-12-22 22:41:31 +0800

    Add two samples

--- src/main/java/com/aliyun/openservices/aliyun/log/producer/sample/SampleProducerWithCallback.java
+++ src/main/java/com/aliyun/openservices/aliyun/log/producer/sample/SampleProducerWithCallback.java
@@ -1,4 +1,98 @@
 package com.aliyun.openservices.aliyun.log.producer.sample;
 
+import com.aliyun.openservices.aliyun.log.producer.Callback;
+import com.aliyun.openservices.aliyun.log.producer.Producer;
+import com.aliyun.openservices.aliyun.log.producer.Result;
+import com.aliyun.openservices.log.common.LogItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class SampleProducerWithCallback {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SampleProducerWithCallback.class);
+
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) throws InterruptedException {
+        final Producer producer = Utils.createProducer();
+
+        int nTask = 100;
+
+        // The monotonically increasing sequence number we will put in the data of each log
+        final AtomicLong sequenceNumber = new AtomicLong(0);
+
+        // The number of logs that have finished (either successfully send, or failed)
+        final AtomicLong completed = new AtomicLong(0);
+
+        final CountDownLatch latch = new CountDownLatch(nTask);
+
+        for (int i = 0; i < nTask; ++i) {
+            EXECUTOR_SERVICE.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        LogItem logItem = Utils.generateLogItem(sequenceNumber.getAndIncrement());
+                        producer.send(
+                                System.getenv("PROJECT"),
+                                System.getenv("LOG_STORE"),
+                                Utils.getTopic(),
+                                Utils.getSource(),
+                                logItem,
+                                new SampleCallback(logItem, completed));
+                    } catch (Exception e) {
+                        LOGGER.error("Failed to put logs, e=", e);
+                    } finally {
+                        latch.countDown();
+                    }
+                }
+            });
+        }
+        latch.await();
+        EXECUTOR_SERVICE.shutdown();
+
+        Utils.doSomething();
+
+        producer.close();
+
+        LOGGER.info("All log complete, completed={}", completed.get());
+    }
+
+    final private static class SampleCallback implements Callback {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(SampleCallback.class);
+
+        private final LogItem logItem;
+
+        private final AtomicLong completed;
+
+        SampleCallback(LogItem logItem, AtomicLong completed) {
+            this.logItem = logItem;
+            this.completed = completed;
+        }
+
+        @Override
+        public void onCompletion(Result result) {
+            try {
+                if (result.isSuccessful()) {
+                    LOGGER.info("Send log successfully, logItem={}", logItem.ToJsonString());
+                } else {
+                    LOGGER.error(
+                            "Failed to send log, logItem={}, errorCode={}, errorMessage={}, attemptCount={}",
+                            logItem.ToJsonString(),
+                            result.getErrorCode(),
+                            result.getErrorMessage(),
+                            result.getAttempts().size());
+                }
+            } finally {
+                completed.getAndIncrement();
+            }
+        }
+    }
+
+
 }
--- src/main/java/com/aliyun/openservices/aliyun/log/producer/sample/SampleProducerWithFuture.java
+++ src/main/java/com/aliyun/openservices/aliyun/log/producer/sample/SampleProducerWithFuture.java
@@ -1,48 +1,88 @@
 package com.aliyun.openservices.aliyun.log.producer.sample;
 
 import com.aliyun.openservices.aliyun.log.producer.*;
+import com.aliyun.openservices.aliyun.log.producer.errors.ResultFailedException;
 import com.aliyun.openservices.log.common.LogItem;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class SampleProducerWithFuture {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SampleProducerWithFuture.class);
 
-    public static void main(String[] args) {
-        Producer producer = createProducer();
-        try {
-            producer.send(System.getenv("PROJECT"), System.getenv("LOG_STORE"), buildLogItem("1"));
-        } catch (InterruptedException e) {
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 
+    public static void main(String[] args) throws InterruptedException {
+        Producer producer = Utils.createProducer();
+        int n = 100;
+
+        // The number of logs that have finished (either successfully send, or failed)
+        final AtomicLong completed = new AtomicLong(0);
+
+        for (int i = 0; i < n; ++i) {
+            ListenableFuture<Result> f;
+            LogItem logItem = Utils.generateLogItem(i);
+            try {
+                f = producer.send(System.getenv("PROJECT"), System.getenv("LOG_STORE"), logItem);
+            } catch (Exception e) {
+                LOGGER.error("Failed to send log, logItem={}, e=", logItem, e);
+                continue;
+            }
+            Futures.addCallback(f, new SampleFutureCallback(logItem, completed), EXECUTOR_SERVICE);
         }
+
+        Utils.doSomething();
+
+        producer.close();
+
+        EXECUTOR_SERVICE.shutdown();
+        while (!EXECUTOR_SERVICE.isTerminated()) {
+            EXECUTOR_SERVICE.awaitTermination(100, TimeUnit.MILLISECONDS);
+        }
+        LOGGER.info("All log complete, completed={}", completed.get());
     }
 
-    public static Producer createProducer() {
-        ProducerConfig producerConfig = new ProducerConfig(buildProjectConfigs());
-        return new LogProducer(producerConfig);
-    }
+    final private static class SampleFutureCallback implements FutureCallback<Result> {
 
-    private static ProjectConfigs buildProjectConfigs() {
-        ProjectConfigs projectConfigs = new ProjectConfigs();
-        projectConfigs.put(buildProjectConfig());
-        return projectConfigs;
-    }
+        private static final Logger LOGGER = LoggerFactory.getLogger(SampleFutureCallback.class);
 
-    private static ProjectConfig buildProjectConfig() {
-        String project = System.getenv("PROJECT");
-        String endpoint = System.getenv("ENDPOINT");
-        String accessKeyId = System.getenv("ACCESS_KEY_ID");
-        String accessKeySecret = System.getenv("ACCESS_KEY_SECRET");
-        return new ProjectConfig(project, endpoint, accessKeyId, accessKeySecret);
-    }
+        private final LogItem logItem;
 
-    public static LogItem buildLogItem(String id) {
-        LogItem logItem = new LogItem();
-        logItem.PushBack("id", id);
-        logItem.PushBack("key1", "val1");
-        logItem.PushBack("key2", "val2");
-        return logItem;
+        private final AtomicLong completed;
+
+        SampleFutureCallback(LogItem logItem, AtomicLong completed) {
+            this.logItem = logItem;
+            this.completed = completed;
+        }
+
+        @Override
+        public void onSuccess(@Nullable Result result) {
+            LOGGER.info("Send log successfully, logItem={}", logItem.ToJsonString());
+            completed.getAndIncrement();
+        }
+
+        @Override
+        public void onFailure(Throwable t) {
+            if (t instanceof ResultFailedException) {
+                Result result = ((ResultFailedException) t).getResult();
+                LOGGER.error(
+                        "Failed to send log, logItem={}, errorCode={}, errorMessage={}, attemptCount={}",
+                        logItem.ToJsonString(),
+                        result.getErrorCode(),
+                        result.getErrorMessage(),
+                        result.getAttempts().size());
+            }
+            completed.getAndIncrement();
+        }
     }
 
 }
